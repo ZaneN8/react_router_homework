af01534f57ba866045d435c64df1b3509b9e2445
killNit <rileysanti@gmail.com>
2020-09-17 16:50:07 -0600

    working on the form

---
+++ src/NewQuote.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+
+class NewQuote extends React.Component {
+  state = { quote: " ", source: " " };
+
+  handleSubmit = (e) => {
+    this.props.addQuote(this.state);
+    this.setState({ quote: " ", source: " "});
+    this.props.addQuote(this.state);
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+
+  render () {
+    return(
+      <Form onSubmit={this.handleSubmit}>
+      <Form.Field>
+        <label>Add Quote</label>
+        <input
+        placeholder='Because I got high...'
+        onChange={this.handleChange}
+        name='quote' 
+        value={this.state.quote}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Source</label>
+        <input 
+        placeholder='Afroman'
+        value={this.state.source}
+        onChange={this.handleChange}
+        name="source"
+         />
+      </Form.Field>
+      <Button color='purple' type='submit'>Submit</Button>
+    </Form>
+
+    )
+  }
+};
+
+
+export default NewQuote;
